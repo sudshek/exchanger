1c669216487ec3719195ad0f6a39c23655542142
tusharsinghal1610 <tusharsinghal309@gmail.com>
2017-10-11 22:45:25 +0530

    first production build

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      "hello world, this is first page"
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route,BrowserRouter,Switch} from 'react-router-dom';
+import App from './components/home';
+
+ReactDOM.render(
+    <BrowserRouter>
+    <Switch>
+          <Route exact path="/" component ={App} />
+    </Switch>
+    </BrowserRouter>,
+    document.getElementById('root'));
